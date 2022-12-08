11a49aaf552fe52b485bc5cd97733548b5f4bcc0
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-12-08 18:23:27 -0300

    Visor component with styling from styled components

--- mobile/src/components/Visor/Visor.js
+++ mobile/src/components/Visor/Visor.js
@@ -1,28 +1,10 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { VisorStyle, TextVisor } from './styles';
 
 export function Visor({ visor }) {
   return (
-    <View style={styles.visor}>
-      <Text style={styles.textVisor}>{visor}</Text>
-    </View>
+    <VisorStyle>
+      <TextVisor>{visor}</TextVisor>
+    </VisorStyle>
   );
 }
-
-const styles = StyleSheet.create({
-  visor: {
-    width: '100%',
-    height: 250,
-    padding: 20,
-    backgroundColor: '#76767676',
-    alignItems: 'flex-end',
-    justifyContent: 'flex-end',
-    borderBottomColor: '#767676',
-    borderBottomWidth: 1,
-  },
-  textVisor: {
-    fontSize: 50,
-    fontFamily: 'SemiBold',
-    color: '#dddd',
-  },
-});
---
+++ mobile/src/components/Visor/styles.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+export const VisorStyle = styled.View`
+  width: 100%;
+  height: 250px;
+  padding-right: 10px;
+  background-color: #76767676;
+  align-items: flex-end;
+  justify-content: flex-end;
+  border-bottom-color: #767676;
+  border-bottom-width: 1px;
+`;
+
+export const TextVisor = styled.Text`
+  font-size: 50px;
+  font-family: SemiBold;
+  color: #dddd;
+`;
